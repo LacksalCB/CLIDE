c7bbbe0aba096e10ff2028a2ebbbbef1a4557699
LacksalCB <3.14159pii@gmail.com>
2026-04-16 17:08:17 -0400

    Update rust for windows

--- src/paths.rs
+++ src/paths.rs
@@ -1,4 +1,6 @@
 use phf::phf_map;
+use std::path::PathBuf;
+use std::env;
 
 static PATHS: phf::Map<&'static str, &str> = phf_map! {
     "linux" => "./local/share/clide",
@@ -6,12 +8,21 @@ static PATHS: phf::Map<&'static str, &str> = phf_map! {
     "mac" => "Library/Application Support/clide",
 };
 
+// %USERPROFILE%
 
-pub fn load_path(file: &str) -> &str {
+
+pub fn load_path(_file: &str) -> &str {
     let os = std::env::consts::OS;    
 
-    if let Some(path) = PATHS.get(os) { 
-        println!("path: {}", path);
+    let home = if os == "windows" {
+        env::var("%USERPROFILE%").unwrap_or_else(|_| "%USERPROFILE%".to_string())
+    } else {
+        env::var("HOME").expect("HOME not set")
+    };
+
+    if let Some(path) = PATHS.get(os) {
+        let prefix = PathBuf::from(&home).join(&path);
+        println!("path: {}", prefix.display());
 
     } else {
         println!("Invalid OS");
