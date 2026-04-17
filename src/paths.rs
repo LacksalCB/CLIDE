a9818ccc84501e957bbf001695de52a72a86211d
LacksalCB <3.14159pii@gmail.com>
2026-04-17 09:51:59 -0400

    Update to better path retreival

--- src/paths.rs
+++ src/paths.rs
@@ -1,23 +1,13 @@
 use std::sync::LazyLock;
 use std::path::PathBuf;
 
+use platform_dirs::AppDirs;
+
 // %USERPROFILE% -> use dir on windows
 
-pub static PREFIX: LazyLock<PathBuf> = LazyLock::new(|| {
-    let home_path = home::home_dir().expect("Could not find home directory"); 
+pub static PREFIX: LazyLock<PathBuf> = LazyLock::new(||  {
+    let prefix = AppDirs::new(Some("clide"), true).unwrap().data_dir;
 
-    #[cfg(target_os="linux")]
-    let path = ".local/share/clide";
-
-    #[cfg(target_os="windows")]
-    let path = "%appdata\\clide"; // Fix for windows one day
-
-    #[cfg(target_os="macos")]
-    let path = "Library/Application Support/clide";
-
-    #[cfg(not(any(target_os="linux", target_os="windows", target_os="macos")))]
-    let path = ".clide";
-   
-    return home_path.join(&path);
+    return prefix;
 });
 
