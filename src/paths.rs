605b37405189e3ab932bab9fca2c5bc8f97f55d1
LacksalCB <3.14159pii@gmail.com>
2026-04-20 14:52:47 -0400

    Update cross-platform check

--- src/paths.rs
+++ src/paths.rs
@@ -4,8 +4,10 @@ use std::path::PathBuf;
 use platform_dirs::AppDirs;
 
 pub static PREFIX: LazyLock<PathBuf> = LazyLock::new(||  {
+    println!("Detected OS: {}", std::env::consts::OS);
     let prefix = AppDirs::new(Some("clide"), true).unwrap().data_dir;
 
+    println!("Detected OS config directory: {}", prefix.display());
     return prefix;
 });
 
