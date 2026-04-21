f0de30aad5e35055ea4f7246dc75ff8cc0b7060f
LacksalCB <3.14159pii@gmail.com>
2026-04-21 18:37:37 -0400

    Update path logic for windows

--- src/paths.rs
+++ src/paths.rs
@@ -4,13 +4,17 @@ use std::path::PathBuf;
 use platform_dirs::AppDirs;
 
 pub static PREFIX: LazyLock<PathBuf> = LazyLock::new(||  {
-    let path = if cfg!(target_os = "macos") {
+    let path = 
+    if cfg!(target_os = "macos") {
         let home = std::env::var("HOME").expect("Home not set");
         PathBuf::from(home)
             .join("Library")
             .join("Application Support")
             .join("clide")
-    } else {
+    } else if cfg!(target_os = "windows") {
+        let roaming = std::env::var("APPDATA").expect("APPDATA not set");
+        PathBuf::from(roaming).join("clide")
+    } else {    
         AppDirs::new(Some("clide"), true).unwrap().data_dir
     };
     path
